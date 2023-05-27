0049de163fccabc10fe2d4175de11405a8eda484
Hoang Tran <tdll.trdphoang@gmail.com>
2023-05-26 23:09:56 -0700

    Implemented navbar

---
+++ promptopia/components/Nav.jsx
@@ -0,0 +1,137 @@
+"use client";
+
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+import { useState, useEffect } from "react";
+import { signIn, signOut, useSession, getProviders } from "next-auth/react";
+
+const Nav = () => {
+	const isUserLoggedIn = true;
+
+	const [providers, setProviders] = useState(null);
+	const [toggleDropdown, setToggleDropdown] = useState(false);
+
+	useEffect(() => {
+		const setProvidersFunction = async () => {
+			const response = await getProviders();
+			setProviders(response);
+		};
+
+		setProvidersFunction();
+	}, []);
+	return (
+		<nav className="flex-between w-full mb-16 pt-3">
+			<Link href="/" className="flex gap-2 flex-center">
+				<Image
+					src="/assets/images/logo.svg"
+					alt="Promptopia logo"
+					width={30}
+					height={30}
+					className="object-contain"
+				/>
+			</Link>
+
+			{/*Desktop Navigation */}
+			<div className="sm:flex hidden">
+				{isUserLoggedIn ? (
+					<div className="flex gap-3 md:gap-5">
+						<Link href="/create-prompt" className="black_btn">
+							Create Posts
+						</Link>
+
+						<button type="button" onClick={signOut} className="outline_btn">
+							Sign Out
+						</button>
+
+						{/*Google logo photo */}
+						<Link href="/profile">
+							<Image
+								src="/assets/images/logo.svg"
+								width={37}
+								height={37}
+								className="rounded-full"
+								alt="profile"
+							/>
+						</Link>
+					</div>
+				) : (
+					<>
+						{providers &&
+							Object.values(providers).map((provider) => (
+								<button
+									type="button"
+									key={provider.name}
+									onClick={() => signIn(provider.id)}
+									className="black_btn"
+								>
+									Sign In
+								</button>
+							))}
+					</>
+				)}
+			</div>
+
+			{/*Mobile Navigation */}
+			<div className="sm:hidden flex relative">
+				{isUserLoggedIn ? (
+					<div className="flex">
+						<Image
+							src="/assets/images/logo.svg"
+							width={37}
+							height={37}
+							className="rounded-full"
+							alt="profile"
+							onClick={() => setToggleDropdown((prev) => !prev)}
+						/>
+
+						{toggleDropdown && (
+							<div className="dropdown">
+								<Link
+									href="/profile"
+									className="dropdown_link"
+									onClick={() => setToggleDropdown(false)}
+								>
+									My Profile
+								</Link>
+								<Link
+									href="/create-prompt"
+									className="dropdown_link"
+									onClick={() => setToggleDropdown(false)}
+								>
+									Create Prompt
+								</Link>
+								<button
+									type="button"
+									onClick={() => {
+										setToggleDropdown(false);
+										signOut();
+									}}
+									className="mt-5 w-full black_btn"
+								>
+									Sign Out
+								</button>
+							</div>
+						)}
+					</div>
+				) : (
+					<>
+						{providers &&
+							Object.values(providers).map((provider) => (
+								<button
+									type="button"
+									key={provider.name}
+									onClick={() => signIn(provider.id)}
+									className="black_btn"
+								>
+									Sign In
+								</button>
+							))}
+					</>
+				)}
+			</div>
+		</nav>
+	);
+};
+
+export default Nav;
