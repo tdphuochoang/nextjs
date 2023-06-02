05f5bbb0a634fec5abf03ebc901f271e73ad8992
Hoang Tran <tdll.trdphoang@gmail.com>
2023-06-01 22:14:08 -0700

    Designed PromptCard component

--- promptopia/components/PromptCard.jsx
+++ promptopia/components/PromptCard.jsx
@@ -6,11 +6,15 @@ import { useSession } from "next-auth/react";
 import { usePathname, useRouter } from "next/navigation";
 
 const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
-	const [copied, setCopied] = useState("");
+	const { data: session } = useSession();
+	const pathName = usePathname();
+	const router = useRouter();
 
+	const [copied, setCopied] = useState("");
+	console.log(post);
 	const handleCopy = () => {
-		setCopied(post[0]?.prompt);
-		navigator.clipboard.writeText(post[0]?.prompt);
+		setCopied(post.prompt);
+		navigator.clipboard.writeText(post.prompt);
 		setTimeout(() => setCopied(""), 3000);
 	};
 	return (
@@ -18,7 +22,7 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
 			<div className="flex justify-between items-start gap-5 ">
 				<div className="flex-1 flex justify-start items-center gap-3 cursor-pointer">
 					<Image
-						src={post[0]?.creator.image}
+						src={post.creator.image}
 						alt="user_image"
 						width={40}
 						height={40}
@@ -27,10 +31,10 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
 
 					<div className="flex flex-col">
 						<h3 className="font-satoshi font-semibold text-gray-900">
-							{post[0]?.creator.username}
+							{post.creator.username}
 						</h3>
 						<p className="font-inter text-sm text-gray-500">
-							{post[0]?.creator.email}
+							{post.creator.email}
 						</p>
 					</div>
 				</div>
@@ -43,25 +47,41 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
 				>
 					<Image
 						src={
-							copied === post[0]?.prompt
+							copied === post.prompt
 								? "/assets/icons/tick.svg"
 								: "/assets/icons/copy.svg"
 						}
 						width={12}
 						height={12}
+						alt=""
 					/>
 				</div>
 			</div>
 
-			<p className="my-4 font-satoshi text-sm text-gray-700">
-				{post[0]?.prompt}
-			</p>
+			<p className="my-4 font-satoshi text-sm text-gray-700">{post.prompt}</p>
 			<p
 				className="font-inter text-sm blue-gradient cursor-pointer"
-				onClick={() => handleTagClick && handleTagClick(post[0].tag)}
+				onClick={() => handleTagClick && handleTagClick(post.tag)}
 			>
-				{post[0]?.tag}
+				#{post.tag}
 			</p>
+
+			{session?.user.id === post.creator._id && pathName === "/profile" && (
+				<div className="mt-5 flex-center gap-4 border-t border-gray-100 pt-3">
+					<p
+						className="font-inter text-sm green_gradient cursor-pointer"
+						onClick={handleEdit}
+					>
+						Edit
+					</p>
+					<p
+						className="font-inter text-sm orange_gradient cursor-pointer"
+						onClick={handleDelete}
+					>
+						Delete
+					</p>
+				</div>
+			)}
 		</div>
 	);
 };
