689bd2ae8340cd1cac426bbfc38568730518444a
Hoang Tran <tdll.trdphoang@gmail.com>
2023-06-11 23:33:03 -0700

    Fixed bug

--- promptopia/components/Profile.jsx
+++ promptopia/components/Profile.jsx
@@ -1,8 +1,6 @@
 import PromptCard from "./PromptCard";
 
 const Profile = ({ name, desc, data, handleEdit, handleDelete }) => {
-	console.log(data);
-
 	return (
 		<section className="w-full">
 			<h1 className="head_text text-left">
